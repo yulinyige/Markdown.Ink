252a8cfbd6482fcb200f3cd8627f9ea4c4820106
yulinyige <yulinyige@163.com>
2021-08-23 23:33:28 +0800

    feat: import antd and craco to support less

---
+++ docs/import-antd-and-less.md
@@ -0,0 +1,15 @@
+# 引入 Antd 和 Less
+
+参考 [antd 官方文档](https://ant.design/docs/react/use-in-typescript-cn)
+
+## 引入 antd
+
+```bash
+$ yarn add antd
+```
+
+## 使用 craco 管理
+
+```bash
+$ yarn add @craco/craco
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import type {FC} from 'react';
 import React from 'react';
-import './App.css';
+import './App.less';
+import {Button} from "antd";
 
 const App: FC = () => {
   return (
     <div className="App">
+      <Button type="dashed">哈哈哈</Button>
     </div>
   );
 }
